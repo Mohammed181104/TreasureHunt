2ad4dcc379b5960f5bcd612bb26a2b0ba8a30b63
Mo <mh210442@reigate.ac.uk>
2021-10-23 19:29:19 +0100

    V1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,7 +33,7 @@ public class Main {
         for (int i = 0; i< 10; i++){
             for (int j = 0; j<10; j++){
                 if (board[i][j].equals("X")) {
-                    System.out.print("[" + board[i][j] + "] ");
+                    System.out.print(" " + board[i][j] + " ");
                 }else{
                     System.out.print("[ ]");
                 }
@@ -41,27 +41,32 @@ public class Main {
             System.out.println(" ");
         }
     }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int points = 0;
-        board();
-        // Assigns items(coins, monsters etc.) to the grid
+    public static void boardItems(){
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                int coinFlip = random.nextInt(6);
-                if (coinFlip == 1 || coinFlip == 3 || coinFlip == 4) {
-                    int coins = random.nextInt(100);
+                int coinFlip = random.nextInt(7);
+                if (coinFlip == 0 || coinFlip == 1 || coinFlip == 2) {
+                    int coins = random.nextInt(200)+1;
                     board[i][j] = String.valueOf(coins);
-                }else if (coinFlip == 2){
+                }else if (coinFlip == 3) {
                     board[i][j] = "M";
+                }else if (coinFlip == 4 || coinFlip == 5){
+                    board[i][j] = "R";
                 }else{
                     board[i][j] = "0";
                 }
             }
         }
+    }
 
-        //cheatBoard();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int money = 0;
+        board();
+        // Assigns items(coins, monsters etc.) to the grid
+        boardItems();
+
+        cheatBoard();
 
         // Main game
         while (true) {
@@ -74,23 +79,38 @@ public class Main {
                 coOrdY = input.nextInt()-1;
             }
             if (!board[coOrdX][coOrdY].equals("0") && !board[coOrdX][coOrdY].equals("X")) {
-                if(board[coOrdX][coOrdY].equals("M")){
+                if(board[coOrdX][coOrdY].equals("M")) {
                     System.out.println("You got trapped by a monster");
                     break;
+                }else if (board[coOrdX][coOrdY].equals("R")){
+                    System.out.println("You have encountered a group of pirates!");
+                    if (money > 300){
+                        int stolen = random.nextInt(75)+100;
+                        System.out.println("The pirates have stolen " + stolen + " coins");
+                        money -= stolen;
+                    }else if((money - 75) <= 0){
+                        System.out.println("The pirates have taken everything!!");
+                        money = 0;
+                        break;
+                    }else{
+                        System.out.println("The pirates stole 75 coins");
+                        money -= 75;
+                    }
+                }else{
+                    money += Integer.parseInt(board[coOrdX][coOrdY]);
+                    System.out.println("You got " + board[coOrdX][coOrdY] + " coin(s)!");
                 }
-                points += Integer.parseInt(board[coOrdX][coOrdY]);
-                System.out.println("You got " + board[coOrdX][coOrdY] + " coin(s)!");
             }else if(board[coOrdX][coOrdY].equals("X")) {
                 System.out.println("You have dug up this spot before, try again.");
             }else{
                 System.out.println("Unlucky, there was nothing there, try again.");
             }
             board[coOrdX][coOrdY] = "X";
-            System.out.println("You have " + points + " coins.");
+            System.out.println("You have " + money + " coins.");
             playerBoard();
 
         }
         System.out.println("GAME OVER");
-        System.out.println("You had " + points + " coins.");
+        System.out.println("You had " + money + " coins.");
     }
 }
